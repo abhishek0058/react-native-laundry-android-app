85a1cc1875cf2e3ea8d79786dbbe5ea5e70a7af2
Abhishek Sharma <abhishek0058@gmail.com>
2019-03-20 21:09:46 +0530

    changed baseurl and app.json

--- components/BaseURL.js
+++ components/BaseURL.js
@@ -1,3 +1,3 @@
-export default BaseURL = `http://laundrybay.in`;
+export default BaseURL = `http://laundrybay.in:3002`;
 // export default BaseURL = `http://192.168.1:3:5000`;
 // export default BaseURL = `http://192.168.43:68:5000`;
