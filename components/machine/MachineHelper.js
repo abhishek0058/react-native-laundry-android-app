45bcc8a2d4a24c3e5e25b24e954206df260af599
Abhishek Sharma <abhishek0058@gmail.com>
2019-05-23 23:31:08 +0530

    failure message change

--- components/machine/MachineHelper.js
+++ components/machine/MachineHelper.js
@@ -65,7 +65,7 @@ export default class MachineHelper extends Component {
             textAlign: "center",
             color: "red"
           }}
-        > Power failure </Text>
+        > Connection failure </Text>
       );
     } else if (machine._status == "active") {
       return (
