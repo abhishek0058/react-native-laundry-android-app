24db3c0b5bf0ce459590acdc4fe9ec1c1bbf4eb1
Abhishek Sharma <abhishek0058@gmail.com>
2019-05-23 23:33:03 +0530

    remove change hostel

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { View, SafeAreaView, ScrollView, Image } from "react-native";
 import { createDrawerNavigator, DrawerItems } from "react-navigation";
-import Profile from "./profile/Profile";
+// import Profile from "./profile/Profile";
 import Package from "./packages/Package";
 import Machine from "./machine/Machine";
-import Verify from "./verify/Verify";
+// import Verify from "./verify/Verify";
 import RefundPolicy from "./refund-policy/RefundPolicy";
 import PurchaseHistory from "./purchaseHistory/PurchaseHistory";
 import Logout from "./Logout";
@@ -12,7 +12,7 @@ import Instructions from './instruction/Instructions';
 import Faq from "./faq/Faq";
 import ContactUs from "./contactus/ContactUs";
 
-import { getData } from "./FetchService";
+// import { getData } from "./FetchService";
 
 const CustomDrawerComponents = props => (
   <SafeAreaView style={{ flex: 1 }}>
@@ -41,7 +41,7 @@ const HomeNavigator = createDrawerNavigator(
     Package: Package,
     Purchases: PurchaseHistory,
     Instructions: Instructions,
-    Profile: Profile,
+    // Profile: Profile,
     Faq: Faq,
     'Our Policies': RefundPolicy,
     'Contact us': ContactUs,
@@ -60,13 +60,7 @@ export default class Home extends Component {
     header: null
   };
 
-  async componentWillMount() {
-    // const id = this.props.navigation.state.params.userid;
-    // const result = await getData(`user/single/${id}`);
-  }
-
   render() {
-    // console.log('home', this.props.navigation.state.params)
     return <HomeNavigator />;
   }
 }
